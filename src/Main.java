154ab8627b47fd348dcc610b0d8794a23046e0db
Thiago Meireles <thiago22md@gmail.com>
2024-02-26 11:59:42 -0300

    lancamento

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("Esse é o ScreenAll");
+        System.out.println("Filme: A volta dos que não foram");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento:" + anoDeLancamento);
+        boolean incluidoNoPlano = true;
         }
     }
